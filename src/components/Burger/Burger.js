38a23f9b4424497ee2bb8bba1f32152b05e8158e
joshgressman <joshgressman@gmail.com>
2018-07-30 05:06:13 -0500

    added map method for dynamic data

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 import classes from './Burger.css';
 
+//receiving props from BurgerBuilder.js
 const burger = (props) => {
+
+  //JS method that extracts keys from an object
+  const transformedIngredients = Object.keys(props.ingredients)
+  .map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+     return <BurgerIngredient key={igKey + i} type={igKey}/>
+    });
+  });
 
   return (
     <div className={classes.Burger}>
      <BurgerIngredient type={"bread-top"}/>
-     <BurgerIngredient type={"cheese"}/>
-     <BurgerIngredient type={"meat"}/>
+     {transformedIngredients}
      <BurgerIngredient type={"bread-bottom"}/>
     </div>
     
