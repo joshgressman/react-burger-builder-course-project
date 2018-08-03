e57b68bb3418542ce531486153642db8f5a315b4
joshgressman <joshgressman@gmail.com>
2018-08-03 13:23:55 -0500

    added disable functionality

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>
       {props.label}
-      <button className={classes.Less} onClick={props.removed}>Less</button>
+      <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
       <button className={classes.More} onClick={props.added}>More</button>
     </div>
   </div>
