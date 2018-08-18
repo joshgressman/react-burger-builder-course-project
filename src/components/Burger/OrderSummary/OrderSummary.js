5fc2d6604b9bc7a139a5eb099b61c5b7ae549ae5
joshgressman <joshgressman@gmail.com>
2018-08-18 09:41:33 -0500

    added modal comp and ing list items

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+  return (
+    <Aux>
+      
+    </Aux>
+  )
+};
+
+export default orderSummary;
