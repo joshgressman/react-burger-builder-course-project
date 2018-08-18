20b962ecc0f23527e979d6539fdea7f69ececbc4
joshgressman <joshgressman@gmail.com>
2018-08-18 09:56:47 -0500

    added modal data

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,9 +2,22 @@ import React from "react";
 import Aux from '../../../hoc/Aux';
 
 const orderSummary = (props) => {
+  //Transform object into an array of items
+  const ingredientSummary = Object.keys(props.ingredients)
+  .map(igKey => {
+    return (
+      <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}:</span>{props.ingredients[igKey]}</li>
+    )
+  });
+
   return (
     <Aux>
-      
+      <h3>Your Order</h3>
+      <p>A Delicouse burger with the following ingredients:</p>
+      <ul>
+       {ingredientSummary}
+      </ul>
+      <p>Continue to checkout?</p>
     </Aux>
   )
 };
